80612b9cda4f86763a97e04cf8b72c61cceae180
SashaViniarWork <alexander.viniar@etiya.com>
2018-07-19 14:30:01 +0300

    first commit and Geolocation Native Geocoder Agm Maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,14 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder} from "@ionic-native/native-geocoder";
+
+import { AgmCoreModule } from '@agm/core';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LocalNotifications} from "@ionic-native/local-notifications";
 
 @NgModule({
   declarations: [
@@ -14,7 +20,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCn09diAFNpdpTcKKrgbOFljzCrUH4tnho'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -23,6 +32,9 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    NativeGeocoder,
+    Geolocation,
+    LocalNotifications,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Geolocation} from '@ionic-native/geolocation';
+import {
+  NativeGeocoder,
+  NativeGeocoderReverseResult,
+  NativeGeocoderForwardResult,
+  NativeGeocoderOptions
+} from '@ionic-native/native-geocoder';
+import {LocalNotifications} from "@ionic-native/local-notifications";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +15,57 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  lat: any;
+  lon: any;
 
+  Location: any;
+  IsLocation: any = false;
+
+  constructor(public navCtrl: NavController, private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder, public local: LocalNotifications) {
+
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = resp.coords.latitude;
+      this.lon = resp.coords.longitude;
+      console.log(this.lon, this.lat);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
   }
 
+  GeocoderWithCoords() {
+
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+    this.nativeGeocoder.reverseGeocode(this.lat, this.lon, options)
+      .then((result: NativeGeocoderReverseResult[]) => {
+        this.Location = result[0];
+        console.log(this.Location);
+        this.IsLocation = true;
+        this.LocalNotification();
+      })
+      .catch((error: any) => console.log(error));
+  }
+
+  LocalNotification() {
+    this.local.schedule({
+      text: this.Location.countryName + ' ' + this.Location.thoroughfare + ' ' + this.Location.subThoroughfare,
+      trigger: {at: new Date(new Date().getTime() + 3600)},
+      led: 'FF0000',
+      sound: null
+
+    });
+
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+    this.nativeGeocoder.forwardGeocode(this.Location.countryName + ' ' + this.Location.thoroughfare + ' ' + this.Location.subThoroughfare, options)
+      .then((coordinates: NativeGeocoderForwardResult[]) => {
+        console.log('The coordinates are latitude=' + coordinates[0].latitude + ' and longitude=' + coordinates[0].longitude);
+      })
+      .catch((error: any) => console.log(error));
+  }
 }
